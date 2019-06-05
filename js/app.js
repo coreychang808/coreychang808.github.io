6efe94ffa0efd292a98dc04bce160ceb7b19af66
corey chang <coreychang808@Hotmail.com>
2019-06-05 10:10:44 -0700

    added char0

--- js/app.js
+++ js/app.js
@@ -2,29 +2,29 @@
 
 var playGame = confirm('Want to play a guessing game about me? If, yes answer in yes/no format please');
 console.log(playGame);
+    
 
-
-  var first = prompt('Am i from Seattle?').toUpperCase();
+  var first = prompt('Am i from Seattle?').charAt(0).toUpperCase();
   console.log(first);
-  if(first === 'YES'){
+  if(first === 'Y'){
   alert("wrong!, im from Hawaii");
   }
   else {
       alert('Dang, you got it');
   }
 
-  var second = prompt('Do i make sushi for a living?').toUpperCase();
+  var second = prompt('Do i make sushi for a living?').charAt(0).toUpperCase();
   console.log(second);
-  if(second === 'YES'){
+  if(second === 'Y'){
       alert('Yes i do!');
   }
   else {
       alert('wrong!, been making sushi for 11 years.');
   }
 
-  var third = prompt('Do i have a 3 year old son?').toUpperCase();
+  var third = prompt('Do i have a 3 year old son?').charAt(0).toUpperCase();
   console.log(third);
-  if(third === 'YES'){
+  if(third === 'Y'){
       alert('false, i have the cutest little girl in the world :)');
   }
   else{
@@ -32,17 +32,17 @@ console.log(playGame);
   }
 
 
-  var fourth = prompt('Do i love to surf and spearfish?').toUpperCase();
+  var fourth = prompt('Do i love to surf and spearfish?').charAt(0).toUpperCase();
   console.log(fourth);
-  if(fourth === 'YES'){
+  if(fourth === 'Y'){
       alert('correct, although i cant do it anymore since i live in Seattle atm');
   }
   else{
       alert('false, surfing and spearfishing is the best!');
   }
-  var fifth = prompt('Am i super cool?').toUpperCase();
+  var fifth = prompt('Am i super cool?').charAt(0).toUpperCase();
   console.log(fifth)
-  if(fifth === 'YES'){
+  if(fifth === 'Y'){
       alert('Good answer!!');
   }
   else{
